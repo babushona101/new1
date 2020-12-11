e0d6f4371443481f5b71a779b9813492787c0e3e
JOHNWICK <johnwick@MACHINE.MACHINEX>
2020-12-11 18:52:07 +0530

    FINDING WITH ABSOLUTE PATH

--- py3.py
+++ py3.py
@@ -2,12 +2,17 @@
 
 import os,sys
 
+#TO lIst the contents of a directory we use os.listdir(directory name
+#TO pRint the path we use os.path.join() which joins the elements in an intelligent way seperating them with /
+#TO fInd absolute path we use os.path.abspath() which provides us with the absolute path
+
 def List(dir):
 	files = os.listdir(dir)
 	for file in files:
 		print("THE OBJ3CT IS ",file)
 		path = os.path.join(dir,file)
 		print("TH3 PATH IS " ,path)
+		print("TH3 ABSOLUTE PATH IS ", os.path.abspath(path))
 		print( "#" * 20 )
 def main():
 	List(sys.argv[1])
